c5a8561a5258a1e34edf0d967a094433ef6ac51e
Cipher95 <h.alobaidan95@gmail.com>
2026-05-03 18:24:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -311,14 +311,14 @@ document.addEventListener('DOMContentLoaded', () => {
                     title: 'World Wide Map',
                     image: '',
                     description: `
-                        <iframe src="https://www.google.com/maps/d/embed?mid=1hjs3mIoZBblBP_CvxiP4w38STiY&hl=en_US&ehbc=2E312F" width="960" height="720"></iframe>
+                        <iframe src="https://www.google.com/maps/d/embed?mid=1hjs3mIoZBblBP_CvxiP4w38STiY&hl=en_US&ehbc=2E312F" width="100%" height="720"></iframe>
                     `
                 },{
                     id: 'Live_Stream_1',
                     title: 'Live Stream (Mega Man Games)',
                     image: '',
                     description: `
-                        <iframe width="560" height="315" src="https://www.youtube.com/embed/NRdwFzYWBbM?si=OVLoPhyxVu3hgZR8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                        <iframe width="100%" height="315" src="https://www.youtube.com/embed/NRdwFzYWBbM?si=OVLoPhyxVu3hgZR8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                     `
                 }
             ]
